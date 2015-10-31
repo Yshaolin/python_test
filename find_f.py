949f98eb5429a2980ec1fc044cece93c5967a789
Yshaolin <yshaolin@outlook.com>
2015-10-31 19:48:32 +0800

    change the struct

--- find_f.py
+++ find_f.py
@@ -4,35 +4,17 @@
 import os
 import os.path, re, sys
 
-args = len(sys.argv)
-if args == 1:
-    print ("Please input the absolute path you want to search:")
-    dir_path = input()
-    print ("Please input the file name you want to find:")
-    file_name = input()
-elif args == 2:
-    print ("Please input the file name you want to find:")
-    file_name = input()
-elif len(sys.argv) == 3:
-    dir_path = argv[1]
-    file_name = argv[2]
-else:
-    sys.exit("Too many paraments!")
-
 L = []
-def find_file(fi, name):
-    if os.path.isdir(fi):
-        for x in os.listdir(fi):
-            find_file(os.path.join(fi, x), name)
-    elif os.path.isfile(fi):
-            patt = re.compile(str(name))
-            if re.search(patt, os.path.split(fi)[1]):
-                L.append(fi)
-    else:
-        pass
+def dir(top, patt, func):
+    if os.path.isdir(top):
+        for x in os.listdir(top):
+            dir(os.path.join(top, x), patt, func)
+    elif os.path.isfile(top):
+        func(top, patt)
 
-if os.path.isdir(dir_path):
-    find_file(dir_path, file_name)
-else:
-    sys.exit("The direc path is Illege!")
-print (L)
+def match_file(file_name, patten):
+    pat = re.compile(patten)
+    if re.match(pat, os.path.split(file_name)[1]):
+        return L.append(file_name)
+
+print dir('.', '.*py', match_file)
