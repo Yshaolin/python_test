d69eaf0e675ca6e63799e7d34c00e03b2d204d41
Yshaolin <yshaolin@outlook.com>
2015-10-31 20:30:09 +0800

    add decorator

--- find_f.py
+++ find_f.py
@@ -5,16 +5,28 @@ import os
 import os.path, re, sys
 
 L = []
-def dir(top, patt, func):
+def dir(top, func):
     if os.path.isdir(top):
         for x in os.listdir(top):
-            dir(os.path.join(top, x), patt, func)
+            dir(os.path.join(top, x), func)
     elif os.path.isfile(top):
-        func(top, patt)
+        func(top)
 
-def match_file(file_name, patten):
-    pat = re.compile(patten)
-    if re.match(pat, os.path.split(file_name)[1]):
-        return L.append(file_name)
+def new_match_file(patten):
+    def decoretor(func):
+        def wrapper(*args, **kw):
+            return func(patten, *args)
+        return wrapper
+    return decoretor
 
-print dir('.', '.*py', match_file)
+
+@new_match_file('test.*')
+def match_file(*args):
+    pat = re.compile(args[0])
+    if re.match(pat, os.path.split(args[1])[1]):
+        return L.append(args[1])
+
+
+
+dir('.', match_file)
+print L
