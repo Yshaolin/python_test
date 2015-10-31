ebc26574a9a595bd647959783df94c6df5c29022
Yshaolin <yshaolin@outlook.com>
2015-10-31 22:21:01 +0800

    create generator

--- find_f.py
+++ find_f.py
@@ -5,28 +5,34 @@ import os
 import os.path, re, sys
 
 L = []
-def dir(top, func):
+def dir(top):
     if os.path.isdir(top):
         for x in os.listdir(top):
-            dir(os.path.join(top, x), func)
+            for i in dir(os.path.join(top, x)):
+                yield i
     elif os.path.isfile(top):
-        func(top)
+        yield top
 
-def new_match_file(patten):
-    def decoretor(func):
-        def wrapper(*args, **kw):
-            return func(patten, *args)
-        return wrapper
-    return decoretor
+#def new_match_file(patten):
+#    def decoretor(func):
+#        def wrapper(*args, **kw):
+#            return func(patten, *args)
+#        return wrapper
+#    return decoretor
 
 
 #@new_match_file('test.*')
 def match_file(top, patt=''):
     def func(top, patten=patt):
         pat = re.compile(patten)
-        print patten,'-'
         if re.match(pat, os.path.split(top)[1]):
             L.append(top)
     return func
-dir('.', match_file('.','test.*'))
+
+f = match_file('.', 'test.*')
+
+x = dir('.')
+for i in x:
+    f(i)
+
 print L
